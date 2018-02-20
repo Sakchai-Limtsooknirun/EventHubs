6e517111fcc95587a81469fece1134070a12dd9f
stampspv <suphavich.c@gmail.com>
2018-02-21 01:05:29 +0700

    ปรับให้เหลือโฟเดอร์เดียว
    ปรับให้เหลือ โฟเดอร์เดียว

--- Register/connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-//$con= mysqli_connect("localhost","admin","jay23513","mydatabase") or die("Error: " . mysqli_error($con));
-$con= mysqli_connect("sql12.freesqldatabase.com","sql12221791","kttagxITs8","sql12221791") or die("Error: " . mysqli_error($con));
-mysqli_query($con, "SET NAMES 'utf8' ");
- 
-?>
