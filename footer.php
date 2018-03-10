f58fc1d9b4c8839b1972b40d587698781cfb025a
stampspv <suphavich.c@gmail.com>
2018-03-11 01:23:09 +0700

    Footer for Organizer Register
    Footer for Organizer Register

---
+++ footer.php
@@ -0,0 +1,54 @@
+<div class='footer'>
+	<a href="javascript:" id="return-to-top"><i class="icon-chevron-up"></i></a>
+	<div class="row">
+		<div class="col-lg-4 footerShow">
+			<h3 style="color:var(--main-color-dark);">ต้องการความช่วยเหลือ?</h3>
+			<p>ติดต่อฝ่ายช่วยเหลือลูกค้า</p>
+			<p>ได้ทุกวันตั้งแต่เวลา 10.00 ถึง 18.00 </p>
+			<small>Tel : 029873721-9</small><br>
+			<small>Email : help@eventhubs.com</small>
+		</div>
+		<div class="col-lg-4 footerShow">
+			<h3 style="color:var(--main-color-dark);">สมัครเป็นผู้จัด</h3>
+			<p>สมัครเป็นผู้จัดออนไลน์<a href="organizer.php">คลิกที่นี่</a></p>
+			<p>หรือติกดต่อฝ่ายจัดกิจกรรม</p>
+			<small>Tel : 0804548452</small><br>
+			<small>Email : ae@eventhubs.com</small>
+		</div>
+		<div class="col-lg-4 footerShow">
+			<h3 style="color:var(--main-color-dark);">Eventhubs</h3>
+			<b>EVENTHUBs CO., LTD.</b><br>
+			<small>50 ถนนงามวงศ์วาน แขวงลาดยาว</small><br>
+			<small>เขตจตุจักร กรุงเทพฯ 10900</small>
+			<p></p>
+			<div class="row">
+
+				<div class="col-lg-1">
+					<p class="fab fa-facebook" style="font-size: 2.5em;"></p>
+				</div>
+				<div class="col-lg-1">
+					<p class="fab fa-twitter-square" style="font-size: 2.5em;"></p>
+				</div>
+				<div class="col-lg-1">
+					<p class="fab fa-instagram" style="font-size: 2.5em;"></p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+<script>
+	// ===== Scroll to Top ====
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+</script>
