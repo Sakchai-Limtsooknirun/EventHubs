ef759a164a575b1005636a203633c0a8aa695f02
stampspv <suphavich.c@gmail.com>
2018-03-12 00:10:46 +0700

    เปลี่ยนรูปหน้าแรก เปลี่ยนรูปสร้างกิจกรรม
    เปลี่ยนรูปหน้าแรก เปลี่ยนรูปสร้างกิจกรรม

--- index.php
+++ index.php
@@ -25,31 +25,27 @@ if ($type == "NotLogin"){
 <div id="captioned-gallery">
     <figure class="slider">
         <figure>
-            <img src="https://images.unsplash.com/photo-1515603403036-f3d35f75ca52?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=49ee68b793d9c0f1f0bc123ffad345e6&auto=format&fit=crop&w=2250&q=80" style="object-fit: cover;filter: brightness(40%);">
-            <div class="textHome">
-                <h1>ค้นหางานกิจกรรมในเรื่องที่คุณชอบ</h1>
-                <?echo $btnHome; ?>
-            </div>
+            <a href="eventview/bnk48"><img src="img/event/bnk48.png" style="object-fit: cover;filter: brightness(90%);"></a>
         </figure>
         <figure>
-            <img src="https://images.unsplash.com/photo-1504680177321-2e6a879aac86?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a8111c411923b7c9dbfc11d87f5d9c48&auto=format&fit=crop&w=1050&q=80" style="object-fit: cover;filter: brightness(40%);">
+            <img src="img/type/concert.jpg" style="object-fit: cover;filter: brightness(40%);">
             <div class="textHome">
                 <h1>ปาร์ตี้มันอยู่ในสายเลือด</h1>
-                <h6>ซื้อบัตรคอนเสริทของวงที่คุณชื่นชอบ</h6>
+                <h5>ซื้อบัตรคอนเสริทของวงที่คุณชื่นชอบ</h5>
             </div>
         </figure>
         <figure>
             <img src="https://images.unsplash.com/photo-1520110120835-c96534a4c984?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a7daa05f48dd9065422a82fda6a6fa71&auto=format&fit=crop&w=1047&q=80" style="object-fit: cover;filter: brightness(40%);">
             <div class="textHome">
                 <h1>อยากเพิ่มความรู่ใส่ตัว ?</h1>
-                <h6>งานสัมนา งานประชุม งานอบรมเราก็มีนะ</h6>
+                <h5>ค้นหางานประชุม งานสัมนา อบรม</h5>
             </div>
         </figure>
         <figure>
             <img src="https://images.unsplash.com/photo-1456613820599-bfe244172af5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ef20eb657eb4d15d1a5154a0246a015e&auto=format&fit=crop&w=1053&q=80" style="object-fit: cover;filter: brightness(40%);">
             <div class="textHome">
                 <h1>อยากออกกำลังกาย ?</h1>
-                <h6>ค้นหากิจกรรมทางกีฬา งานแข่ง</h6>
+                <h5>ค้นหากิจกรรมทางกีฬา งานแข่ง</h5>
             </div>
         </figure>
         <figure>
