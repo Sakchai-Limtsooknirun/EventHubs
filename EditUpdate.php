5460445b8c9ff07a59fb56ab6d0ada935bef7706
b5810451152 <anaphat.i@ku.th>
2018-03-11 14:50:27 +0700

    ..

--- EditUpdate.php
+++ EditUpdate.php
@@ -6,14 +6,14 @@ include 'connection.php';
  if (isset($_SESSION["Username"])){
     if (isset($_SESSION['frmAction']) == isset($_POST['frmAction'])) {
         $statusPic = getOneValue("SELECT `Picture` AS 'get' FROM `user` WHERE `Username` = '{$_SESSION['Username']}'");
-        
+
         if ($_FILES['editPic']['error'] == "0") {
              $file_name     = $_FILES['editPic']['name'];
         }else{
             if($statusPic == ""){
             $file_name     = "Default.png";
             }else {
-                $file_name =  $statusPic; 
+                $file_name =  $statusPic;
             }
          }
 
@@ -38,10 +38,10 @@ include 'connection.php';
         // }else{
             $meSQL .= "Picture='{$file_name}' ";
         //}
-        
+
         $meSQL .= "WHERE Username ='{$_SESSION['Username']}' ";
-        
-       
+
+
         if (isset($_FILES['editPic'])) {
             echo "have image";
             $errors    = array();
@@ -50,17 +50,17 @@ include 'connection.php';
             $file_tmp  = $_FILES['editPic']['tmp_name'];
             $file_type = $_FILES['editPic']['type'];
             $file_ext  = strtolower(end(explode('.', $_FILES['editPic']['name'])));
-        
+
             $expensions = array("jpeg", "jpg", "png");
-        
+
             if (in_array($file_ext, $expensions) === false) {
                 $errors[] = "extension not allowed, please choose a JPEG or PNG file.";
             }
-        
+
             if ($file_size > 2097152) {
                 $errors[] = 'File size must be excately 2 MB';
             }
-        
+
             if (empty($errors) == true) {
                 move_uploaded_file($file_tmp, "img/user/" . $file_name);
                 echo "Success";
@@ -82,13 +82,13 @@ include 'connection.php';
                 echo "</script>";
             }
             mysql_close();
-            } 
+            }
         else {
             echo "<script type='text/javascript'>";
             echo "window.location = 'Edit.php'; ";
             echo "</script>";
             }
-         } 
+         }
     else {
             echo "<script type='text/javascript'>";
             echo "window.location = 'login.php'; ";
@@ -97,6 +97,3 @@ include 'connection.php';
 
 mysqli_close($con);
 ?>
-        
-
- 
